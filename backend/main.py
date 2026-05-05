4c98d7c3fdd29f208f12543b1ab3ee648e5533f3
vladlenblch <vlad6larionov@gmail.com>
2026-05-05 21:11:14 +0300

    basic server

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Backend API is running"}
